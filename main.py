80142ed93ef35127d890c26d674a1aab30c09e9f
ppaanngggg <ppaanngggg@gmail.com>
2014-07-25 18:12:40 +0800

    test SSH

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from Tkinter import *
-root=Tk()
-w=Label(root,text="hello world!")
-w.pack()
+
+
+class App:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(
+            frame, text="quit", fg="red", command=frame.quit
+        )
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(
+            frame, text="hello", command=self.say_hi
+        )
+        self.hi_there.pack(side=LEFT)
+
+    def say_hi(self):
+        print "hi there,everyone!"
+
+
+root = Tk()
+app = App(root)
+
 root.mainloop()
+root.destroy()
