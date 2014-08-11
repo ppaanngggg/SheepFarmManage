563fe4d103074ac6373add8778292aee9d1c09ce
ppaanngggg <ppaanngggg@gmail.com>
2014-08-11 19:58:28 +0800

    1.add_CHAN_GAO; 2.createtable.sql;

---
+++ createtable.sql
@@ -0,0 +1,33 @@
+create table chan_gao
+(
+	chan_gao_hao char(10) not null primary key,
+	peng_hao char(5) not null,
+	lan_hao char(5) not null,
+	mu_yang_hao char(10) not null,
+	gong_yang_hao char(10) not null,
+	tai_ci int,
+	pei_zhong_ri_qi date,
+	chan_gao_ri_qi date not null,
+	chan_gao int not null,
+	huo_gao int not null,
+	duan_nai_ri_qi date
+);
+
+create table yang
+(
+	bian_hao char(10) not null primary key,
+	xing_bie char(5) not null,
+	er_hao char(10),
+	chu_sheng_zhong float(10,1),
+	duan_nai_zhong float(10,1),
+	liu_yue_zhong float(10,1),
+	zhou_sui_zhong float(10,1),
+	qu_xiang char(20),
+	chan_gao_bian_hao char(10)
+);
+
+create table chan_yang_guan_xi
+(
+	chan_gao_hao char(10) not null,
+	bian_hao char(10) not null
+);
--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from PyQt5.QtWidgets import *
 
+from add_CHAN_GAO import *
 
 class CMainWindow(QWidget):
     def __init__(self, parent=None):
         super(CMainWindow, self).__init__(parent)
 
-        self.main_layout=QVBoxLayout()
+        self.main_title=QLabel('欢迎使用湖羊数据管理系统')
 
+        self.button_add_CHAN_GAO=QPushButton('添加产羔记录')
+        self.button_add_CHAN_GAO.clicked.connect(self.button_add_CHAN_GAO_clicked)
+
+        self.button_add_YANG=QPushButton('添加湖羊数据')
+
+        self.main_layout=QVBoxLayout()
+        self.main_layout.addWidget(self.main_title)
+        self.main_layout.addWidget(self.button_add_CHAN_GAO)
+        self.main_layout.addWidget(self.button_add_YANG)
 
         self.setLayout(self.main_layout)
+
+    def button_add_CHAN_GAO_clicked(self):
+        self.add_CHAN_GAO_dialog=CAdd_CHAN_GAO_Dialog()
+        self.add_CHAN_GAO_dialog.show()
 
 if __name__ == '__main__':
     import sys
