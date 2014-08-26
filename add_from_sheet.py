0a7ec1c0484ddc25b15e0f0ffdecc71578820696
ppaanngggg <ppaanngggg@gmail.com>
2014-08-26 22:36:19 +0800

    fix some bug
    mainly def cell2str(book,cell): …

--- add_from_sheet.py
+++ add_from_sheet.py
@@ -5,15 +5,22 @@ from mysql.connector import errorcode
 from PyQt5.QtWidgets import QMessageBox
 
 
-def num2str(num):
-    s = str(num)
-    if s.endswith(".0"):
-        s = s[:-2]
-    return s
+def cell2str(book, cell):
+    if cell.ctype == xlrd.XL_CELL_NUMBER and int(cell.value) == cell.value:
+        xf = book.xf_list[cell.xf_index]
+        format = book.format_map[xf.format_key]
+        format_str = format.format_str
+        int_str = str(int(cell.value))
+        if all(x=='0' for x in format_str):
+            return '0' * (len(format_str) - len(int_str)) + int_str
+        else:
+            return int_str
+    else:
+        return str(cell.value)
 
 
 def add_from_sheet(str_path, QWidget):
-    book = xlrd.open_workbook(str_path)
+    book = xlrd.open_workbook(str_path, formatting_info=True)
 
     mysql_query = []
 
@@ -23,21 +30,21 @@ def add_from_sheet(str_path, QWidget):
         duan_nai_ri_qi = 0
 
         for row_index in range(3, sheet.nrows):
-            mu_yang_hao = num2str(sheet.cell(row_index, 2).value)
-            gong_yang_hao = num2str(sheet.cell(row_index, 3).value)
+            mu_yang_hao = cell2str(book, sheet.cell(row_index, 2))
+            gong_yang_hao = cell2str(book, sheet.cell(row_index, 3))
             if mu_yang_hao and gong_yang_hao and not mu_yang_hao.isspace() and not gong_yang_hao.isspace():
                 try:
                     # print(sheet.name,row_index)
                     # peng_hao
-                    peng_hao_tmp = num2str(sheet.cell(row_index, 0).value)
+                    peng_hao_tmp = cell2str(book, sheet.cell(row_index, 0))
                     if peng_hao_tmp and not peng_hao_tmp.isspace():
                         peng_hao = peng_hao_tmp
                     # lan_hao
-                    lan_hao_tmp = num2str(sheet.cell(row_index, 1).value)
+                    lan_hao_tmp = cell2str(book, sheet.cell(row_index, 1))
                     if lan_hao_tmp and not lan_hao_tmp.isspace():
                         lan_hao = lan_hao_tmp
                     # tai_ci
-                    tai_ci = num2str(sheet.cell(row_index, 4).value)
+                    tai_ci = cell2str(book, sheet.cell(row_index, 4))
                     # pei_zhong_ri_qi
                     pei_zhong_ri_qi = str(sheet.cell(row_index, 5).value)
                     if pei_zhong_ri_qi and not pei_zhong_ri_qi.isspace():
@@ -45,12 +52,14 @@ def add_from_sheet(str_path, QWidget):
                         pei_zhong_ri_qi = date(year=pei_zhong_ri_qi[0], month=pei_zhong_ri_qi[1],
                                                day=pei_zhong_ri_qi[2])
                     # chan_gao_ri_qi
-                    chan_gao_ri_qi = xlrd.xldate_as_tuple(sheet.cell(row_index, 6).value, 0)
-                    chan_gao_ri_qi = date(year=chan_gao_ri_qi[0], month=chan_gao_ri_qi[1], day=chan_gao_ri_qi[2])
+                    chan_gao_ri_qi = str(sheet.cell(row_index, 6).value)
+                    if chan_gao_ri_qi and not chan_gao_ri_qi.isspace():
+                        chan_gao_ri_qi = xlrd.xldate_as_tuple(sheet.cell(row_index, 6).value, 0)
+                        chan_gao_ri_qi = date(year=chan_gao_ri_qi[0], month=chan_gao_ri_qi[1], day=chan_gao_ri_qi[2])
                     # chan_gao
-                    chan_gao = num2str(sheet.cell(row_index, 7).value)
+                    chan_gao = cell2str(book, sheet.cell(row_index, 7))
                     # huo_gao
-                    huo_gao = num2str(sheet.cell(row_index, 8).value)
+                    huo_gao = cell2str(book, sheet.cell(row_index, 8))
                     # duan_nai_ri_qi
                     duan_nai_ri_qi_tmp = str(sheet.cell(row_index, 15).value)
                     if duan_nai_ri_qi_tmp and not duan_nai_ri_qi_tmp.isspace():
@@ -58,8 +67,8 @@ def add_from_sheet(str_path, QWidget):
                         duan_nai_ri_qi = date(year=duan_nai_ri_qi[0], month=duan_nai_ri_qi[1], day=duan_nai_ri_qi[2])
                     # chan_gao_hao
                     chan_gao_hao = str(chan_gao_ri_qi) + mu_yang_hao + gong_yang_hao
-                    # print(chan_gao_hao, peng_hao, lan_hao, mu_yang_hao, gong_yang_hao, tai_ci, pei_zhong_ri_qi, chan_gao_ri_qi,
-                    # chan_gao, huo_gao, duan_nai_ri_qi)
+                    # print(chan_gao_hao, peng_hao, lan_hao, mu_yang_hao, gong_yang_hao, tai_ci, pei_zhong_ri_qi,
+                    #       chan_gao_ri_qi, chan_gao, huo_gao, duan_nai_ri_qi)
 
                     query_index = 'insert into chan_gao ('
                     query_values = ') values ('
@@ -105,7 +114,7 @@ def add_from_sheet(str_path, QWidget):
                     for row_offset in range(0, 2):
                         for col_offset in range(0, 2):
                             if row_index + row_offset < sheet.nrows:
-                                bian_hao = num2str(sheet.cell(row_index + row_offset, 9 + col_offset * 3).value)
+                                bian_hao = cell2str(book, sheet.cell(row_index + row_offset, 9 + col_offset * 3))
                                 if bian_hao and not bian_hao.isspace():
                                     chu_sheng_zhong = str(sheet.cell(row_index + row_offset, 10 + col_offset * 3).value)
                                     duan_nai_zhong = str(sheet.cell(row_index + row_offset, 11 + col_offset * 3).value)
@@ -160,13 +169,13 @@ def add_from_sheet(str_path, QWidget):
                     cursor.execute(query)
                     cnx.commit()
                 except mysql.connector.Error as err:
-                    # print(query)
+                    print(query)
                     if err.errno == errorcode.ER_PARSE_ERROR:
-                        QMessageBox.information(QWidget, '数据库错误', '无数据输入。\n'+query)
+                        QMessageBox.information(QWidget, '数据库错误', '无数据输入。\n' + query)
                     elif err.errno == errorcode.ER_DUP_ENTRY:
-                        QMessageBox.information(QWidget, '数据库错误', '产羔号或编号重复。\n'+query)
-                    elif err.errno ==errorcode.ER_BAD_FIELD_ERROR:
-                        QMessageBox.information(QWidget, '数据库错误', '数据类型有误。\n'+query)
+                        QMessageBox.information(QWidget, '数据库错误', '产羔号或编号重复。\n' + query)
+                    elif err.errno == errorcode.ER_BAD_FIELD_ERROR:
+                        QMessageBox.information(QWidget, '数据库错误', '数据类型有误。\n' + query)
             cursor.close()
             cnx.close()
             QMessageBox.information(QWidget, '从表格导入数据', '导入成功。')
